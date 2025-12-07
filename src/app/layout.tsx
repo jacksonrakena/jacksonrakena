8c86dca005eaa52391585e1b9479b31e3c3ea362
jacksonrakena <jackson@rakena.co.nz>
2025-12-07 15:14:06 +1100

    remove ugly theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ThemeProvider attribute={'class'}>
-          <Theme accentColor="mint">
+          <Theme>
             {children}
           </Theme>
         </ThemeProvider>
